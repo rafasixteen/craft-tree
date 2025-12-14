b18ad7284093aca94db257eb187487c3d0b2556f
DESKTOP-N2SOVPU\rafae <rafaelrego16@gmail.com>
2025-12-14 01:59:23 +0000

    removed old item tree and node versions.

--- src/components/Items/index.ts
+++ src/components/Items/index.ts
@@ -1,4 +1,2 @@
-export { default as ItemTree } from './ItemTree';
-export { default as NodeRenderer } from './NodeRenderer';
 export { default as ItemTreeV2 } from './ItemTreeV2';
 export { default as NodeRendererV2 } from './NodeRendererV2';
