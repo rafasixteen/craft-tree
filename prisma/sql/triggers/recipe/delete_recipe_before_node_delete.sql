f276f2a853adc044dda9d6df2b6661cfce59545c
rafasixteen <rafaelrego16@gmail.com>
2025-12-18 01:35:35 +0000

    fixed recipe and item not being deleted when their related node was deleted

--- prisma/sql/triggers/recipe/delete_recipe_before_node_delete.sql
+++ prisma/sql/triggers/recipe/delete_recipe_before_node_delete.sql
@@ -1,6 +1,6 @@
 DROP TRIGGER IF EXISTS delete_recipe_before_node_delete ON "Nodes";
 
 CREATE TRIGGER delete_recipe_before_node_delete
-BEFORE DELETE ON "Nodes"
+AFTER DELETE ON "Nodes"
 FOR EACH ROW
 EXECUTE FUNCTION delete_recipe_before_node_delete();
