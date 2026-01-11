94ea7a8b4d22416c9a24c3bb3fffad617c1b57c5
rafasixteen-desktop <rafaelrego16@gmail.com>
2026-01-11 04:18:44 +0000

    fixed header layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,12 +6,14 @@ import { SessionProvider } from 'next-auth/react';
 export function Header()
 {
 	return (
-		<header className="flex items-center px-4 py-2 border-b justify-between h-16">
+		<header className="flex items-center justify-between px-4 py-2 border-b h-16">
 			<Navbar />
-			<ThemeToggle />
-			<SessionProvider>
-				<User />
-			</SessionProvider>
+			<div className="flex items-center gap-4">
+				<ThemeToggle />
+				<SessionProvider>
+					<User />
+				</SessionProvider>
+			</div>
 		</header>
 	);
 }
