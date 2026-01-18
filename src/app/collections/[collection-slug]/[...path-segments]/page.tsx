c304395ab2028ac9fd5747ec3195ad8f0e6d824c
rafasixteen-desktop <rafaelrego16@gmail.com>
2026-01-18 16:23:12 +0000

    finished catch all route

--- src/app/collections/[collection-slug]/[...path-segments]/page.tsx
+++ src/app/collections/[collection-slug]/[...path-segments]/page.tsx
@@ -1,22 +1,15 @@
 interface PageProps
 {
-	params: Promise<{
-		'collection-slug': string;
-		'path-segments': string[];
-	}>;
+	params: Promise<{ 'collection-slug': string; 'path-segments': string[] }>;
 }
 
 export default async function Page({ params }: PageProps)
 {
-	const { 'collection-slug': collectionSlug, 'path-segments': pathSegments } = await params;
-
-	console.log('Collection:', collectionSlug);
-	console.log('Path segments:', pathSegments);
+	const { 'path-segments': pathSegments, 'collection-slug': collectionSlug } = await params;
 
 	return (
-		<div>
-			<h1>Collection: {collectionSlug}</h1>
-			<p>Path segments: {pathSegments.join(' / ') || '(none)'}</p>
-		</div>
+		<p>
+			Item Page: {pathSegments.join('/')} at {collectionSlug}
+		</p>
 	);
 }
