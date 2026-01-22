ce4a1430be95d8064cc4bd4346de925dca91731e
rafasixteen-desktop <rafaelrego16@gmail.com>
2026-01-22 04:14:32 +0000

    removed graphql

--- src/graphql/resolvers/index.ts
+++
@@ -1,5 +0,0 @@
-import { collectionResolvers } from './collections';
-import { itemResolvers } from './items';
-import { recipeResolvers } from './recipes';
-
-export const resolvers = [collectionResolvers, itemResolvers, recipeResolvers];
