93945a688668f95048ae9df7a01f5bc9ad435970
rafasixteen-laptop <rafaelrego16@gmail.com>
2026-02-16 16:45:26 +0000

    changed link to include /items

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -9,10 +9,11 @@ export default function InventoryRootPage()
 
 	if (inventories.length > 0)
 	{
+		// TODO: Redirect either to /items or /producers based on the last visited page for that inventory.
 		return inventories.map((inventory) => (
 			<div key={inventory.id} className="flex flex-1 flex-col gap-4 p-4">
 				<Link
-					href={`/inventory/${inventory.id}`}
+					href={`/inventory/${inventory.id}/items`}
 					className="inline-flex items-center rounded-md border border-input bg-background px-4 py-2 text-base font-medium shadow-sm transition-colors hover:bg-accent focus:ring-2 focus:ring-ring focus:ring-offset-2 focus:outline-none"
 				>
 					{inventory.name}
